b6f9bcf6a3664db066e76f8129dfb426384fe767
Soumyadeep Dey <83417245+Soumyadeep-Dey123@users.noreply.github.com>
2024-01-12 22:50:09 +0530

    Add files via upload

--- courses.js
+++ courses.js
@@ -6,3 +6,43 @@ document.addEventListener("DOMContentLoaded", function () {
       navbar.classList.toggle("active");
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var chapters = document.querySelectorAll('.chapter-box');
+
+    chapters.forEach(function (chapter) {
+        chapter.addEventListener("click", function () {
+            var dropdown = chapter.querySelector('.fa-caret-down, .fa-caret-up');
+            
+            // Toggle dropdown icon
+            if (dropdown) {
+                dropdown.classList.toggle("fa-caret-down");
+                dropdown.classList.toggle("fa-caret-up");
+            }
+
+            // Toggle visibility of lecture siblings
+            var sibling = chapter.nextElementSibling;
+            while (sibling && !sibling.classList.contains('chapter-box')) {
+                // Check if the sibling has the class 'chapter-lecture'
+                if (sibling.classList.contains('course-lecture')) {
+                    sibling.style.display = dropdown.classList.contains("fa-caret-up") ? 'flex' : 'none';
+                }
+                sibling = sibling.nextElementSibling;
+            }
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    
+ var buttontest = document.querySelector(".test-button");
+ var buttonbuy = document.querySelector(".buy-now-button");
+    buttonbuy.addEventListener("click", function () {
+    buttonbuy.style.display="none";
+    
+    });
+  });
+
+
+
+   
+  
